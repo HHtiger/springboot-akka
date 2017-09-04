0eb045d00e84e6452f2589582422a426594b1f2d
hehu <872749212@qq.com>
2017-09-04 16:58:19 +0000

    spring boot mock

--- springboot/src/test/java/com/tiger/controller/SampleControllerTest2SpringBoot.java
+++ springboot/src/test/java/com/tiger/controller/SampleControllerTest2SpringBoot.java
@@ -2,16 +2,12 @@ package com.tiger.controller;
 
 
 import com.tiger.com.tiger.PicServer;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
@@ -19,28 +15,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ComponentScan(
-        basePackages = "com.tiger",
-        excludeFilters = {@ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.tiger.model")}
-)
-public class SampleControllerTest {
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest2SpringBoot {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private PicServer picServer;
-
-    @InjectMocks
-    private SampleController sampleController;
-
-    @Before
-    public void before() {
-        this.mockMvc = standaloneSetup(sampleController).build();
-    }
 
     @Test
     public void testGetPic() throws Exception {
@@ -49,10 +33,11 @@ public class SampleControllerTest {
 
         when(picServer.getPic()).thenReturn(testRes);
 
-        mockMvc.perform(get("/getPic").accept(MediaType.APPLICATION_JSON_UTF8))
+        mockMvc.perform(get("/getPic"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(testRes));
+
     }
 
 }
